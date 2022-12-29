7604d430341b5c59abee19f2032a5002ba380c0c
faustinopieroni1 <110001449+faustinopieroni1@users.noreply.github.com>
2022-12-29 12:57:30 -0300

    x

--- src/eventos/Input.jsx
+++ src/eventos/Input.jsx
@@ -2,21 +2,34 @@
 
 function Input() {
 
-    function inputHandler(evento) {
+    
+    /* function inputHandler(evento) {
         console.log(evento.nativeEvent)  //---> .nativeEvent sirve para acceder a la info del evento
-    }
+    } */
 
-    function inputHandler2(evento){
-        console.log(evento.key) //---> .key muestra las lo que se ingresa en el input
+
+    function inputHandler2(evento) {
+
+        let vocales=['a','e','i','o','u']
+
+        if (vocales.includes(evento.key)) {
+
+            evento.preventDefault() //---> .preventDefault cancela la funcion de algunos eventos 
+            console.log(evento.key) //---> .key muestra lo que se ingresa en el input
+        }
+
     }
 
 
 
     return (
         <div className="mt-5">
-            <input type="text" placeholder="(:" onKeyDown={inputHandler2} /* onClick={inputHandler} */ /> 
+            <input type="text" placeholder="(:" onKeyDown={inputHandler2} />
         </div>
     )
 }
 
 export default Input
+
+
+/* (evento.key == 'a' || evento.key == 'e' || evento.key == 'i' || evento.key == 'o' || evento.key == 'u') */
